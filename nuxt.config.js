073542ff46d2a5e9f2447f3b0c69f9fa5020fbf9
Diego Orellana <dev@dorellango.cl>
2019-08-18 15:31:41 -0400

    favicon added

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
     ],
     link: [
       {
-        rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
+        rel: 'icon', type: 'image/png', href: '/favicon.png'
       },
       {
         rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Kalam&display=swap'
